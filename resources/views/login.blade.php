8ac11c7def215ec869af9f08b085b7fbac4d9282
krupalpiprotar20 <krupalpiprotar@gmail.com>
2024-02-26 11:24:57 +0530

    Login Functionality Done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,18 +6,19 @@
       
         <div class="row justify-content-center">
            <div class="col-sm-4">
-            <form>
+            <form action="login" method="POST">
+              @csrf
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
                   
                 </div>
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="password" class="form-control" id="password" name="password">
                 </div>
                 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-success">Login</button>
               </form>
            </div>
         </div>
